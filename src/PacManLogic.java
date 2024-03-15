104da05930e12a0d0e0b092e56b0249e0f104ed3
BT_1N3_22 <sample@bths.edu>
2024-03-15 10:55:19 -0400

    my first change

--- src/PacManLogic.java
+++ src/PacManLogic.java
@@ -14,11 +14,23 @@ public class PacManLogic {
     }
 
     private void setUpMaze() {
+        for (int i = 0; i < 150; i++) {
+            maze[0][i] = new Spaces("⬜");
+            maze[maze.length - 1][i] = new Spaces("⬜");
+        }
+        for (int i = 0; i < maze.length; i++) {
+            maze[i][0] = new Spaces("⬜");
+            maze[i][maze[0].length - 1] = new Spaces("⬜");
+        }
         for (int r = 0; r < maze.length; r++) {
             for (int c = 0; c < maze[r].length; c++) {
-                maze[r][c] = new Spaces("_");
+                if (maze[r][c] == null) {
+                    maze[r][c] = new Spaces("-");
+                }
+
             }
         }
+        maze[3][5] = new Spaces("ᗧ");
     }
 
     private void printGrid() {
